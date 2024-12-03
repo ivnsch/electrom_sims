b9e3722865c2d7d8b94ed42518d793cedff3b58e
Ivan Schuetz <ivanschuetz@gmail.com>
2024-12-03 17:33:51 +0100

    use rendering loop and objects

--- src/coulomb.ts
+++ src/coulomb.ts
@@ -1,4 +1,49 @@
-import { drawCircle } from "./draw.js";
+type Circle = DrawableInterface & {
+  radius: number;
+  type: DrawableType.Circle;
+};
+
+type Tbd = DrawableInterface & {
+  type: DrawableType.Tbd;
+};
+
+interface DrawableInterface {
+  x: number;
+  y: number;
+  type: DrawableType;
+}
+
+enum DrawableType {
+  Circle,
+  Tbd,
+}
+
+type Drawable = Circle | Tbd;
+
+const update = (objs: Drawable[]) => {};
+
+const draw = (ctx: CanvasRenderingContext2D, objs: Drawable[]) => {
+  objs.forEach((o) => {
+    renderObj(ctx, o);
+  });
+};
+
+const renderObj = (ctx: CanvasRenderingContext2D, obj: Drawable) => {
+  switch (obj.type) {
+    case DrawableType.Circle: {
+      ctx.beginPath();
+      ctx.arc(obj.x, obj.y, obj.radius, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
+    }
+  }
+};
+
+const simLoop = (ctx: CanvasRenderingContext2D, objs: Drawable[]) => {
+  update(objs);
+  draw(ctx, objs);
+  requestAnimationFrame(() => simLoop(ctx, objs));
+};
 
 const run = (document: Document): void => {
   const canvas = document.getElementById("mycanvas") as HTMLCanvasElement;
@@ -11,8 +56,12 @@ const run = (document: Document): void => {
     throw new Error("2D context not available");
   }
 
-  drawCircle(ctx, 200, 100);
-  drawCircle(ctx, 300, 200);
+  const objs = [
+    { x: 200, y: 100, radius: 20, type: DrawableType.Circle },
+    { x: 300, y: 200, radius: 20, type: DrawableType.Circle },
+  ];
+
+  simLoop(ctx, objs);
 };
 
 run(document);
