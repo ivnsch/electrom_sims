479a5a7af1126ad0bd483c48d8c2935e328f4fc2
Ivan Schuetz <ivanschuetz@gmail.com>
2024-12-06 19:02:19 +0100

    move to fn

--- src/dim3.ts
+++ src/dim3.ts
@@ -1,6 +1,29 @@
 import { getContext } from "./common_html.js";
 import { drawCircle } from "./draw.js";
 
+type Point3d = { x: number; y: number; z: number };
+type Point2d = { x: number; y: number };
+
+const toScreenCoords = (modelCoords: Point3d): Point2d => {
+  // defined in html
+  const width = 1000;
+  const height = 600;
+
+  const aspectRatio = width / height;
+
+  // project onto canvas
+  const xProj = modelCoords.x / modelCoords.z;
+  const yProj = (modelCoords.y / modelCoords.z) * aspectRatio;
+  // normalize ([-1, 1] -> [0, 1])
+  const xProjRemap = (1 + xProj) / 2;
+  const yProjRemap = (1 + yProj) / 2;
+  // normalized -> full canvas space
+  const xProjPix = xProjRemap * width;
+  const yProjPix = yProjRemap * height;
+
+  return { x: xProjPix, y: yProjPix };
+};
+
 const render = (document: Document): void => {
   const ctx = getContext(document);
 
@@ -15,24 +38,10 @@ const render = (document: Document): void => {
     { x: -1, y: 1, z: -3 },
   ];
 
-  // defined in html
-  const width = 1000;
-  const height = 600;
-
-  const aspectRatio = width / height;
-
   for (const corner of corners) {
-    // project onto canvas
-    const xProj = corner.x / corner.z;
-    const yProj = (corner.y / corner.z) * aspectRatio;
-    // normalize ([-1, 1] -> [0, 1])
-    const xProjRemap = (1 + xProj) / 2;
-    const yProjRemap = (1 + yProj) / 2;
-    // normalized -> full canvas space
-    const xProjPix = xProjRemap * width;
-    const yProjPix = yProjRemap * height;
+    let screenCoords = toScreenCoords(corner);
 
-    drawCircle(ctx, xProjPix, yProjPix, 5);
+    drawCircle(ctx, screenCoords.x, screenCoords.y, 5);
   }
 };
 
