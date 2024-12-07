53949bdb4f276553e0fe0edd6f1b0f316b5c95f5
Ivan Schuetz <ivanschuetz@gmail.com>
2024-12-07 08:38:51 +0100

    render slices

--- src/spheric_coords.ts
+++ src/spheric_coords.ts
@@ -3,14 +3,44 @@ import { getContext } from "./common_html.js";
 import { drawCircle } from "./draw.js";
 import { Vec3 } from "./vec3.js";
 
+// iterate through spheric coords to make a sphere
 const renderSphere = (ctx: CanvasRenderingContext2D): void => {
-  // iterate through spheric coords to make a sphere
   let r = 1;
 
   // divide by a very large value (e.g. 1000) to see a solid sphere
   let step = (Math.PI * 2) / 50;
   for (let t = 0; t < Math.PI * 2; t += step) {
     for (let p = 0; p < Math.PI * 2; p += step) {
+      const cartesian = toCartesian({ r: r, theta: t, phi: p });
+      cartesian.z -= 3;
+
+      const screenCoords = toScreenCoords(cartesian);
+      drawCircle(ctx, screenCoords.x, screenCoords.y, 5);
+    }
+  }
+};
+
+const renderThetaSlice = (ctx: CanvasRenderingContext2D): void => {
+  let r = 1;
+
+  let step = (Math.PI * 2) / 50;
+  for (let t = step * 3; t < step * 4; t += step) {
+    for (let p = 0; p < Math.PI * 2; p += step) {
+      const cartesian = toCartesian({ r: r, theta: t, phi: p });
+      cartesian.z -= 3;
+
+      const screenCoords = toScreenCoords(cartesian);
+      drawCircle(ctx, screenCoords.x, screenCoords.y, 5);
+    }
+  }
+};
+
+const renderPhiSlice = (ctx: CanvasRenderingContext2D): void => {
+  let r = 1;
+
+  let step = (Math.PI * 2) / 50;
+  for (let t = 0; t < Math.PI * 2; t += step) {
+    for (let p = step * 3; p < step * 4; p += step) {
       const cartesian = toCartesian({ r: r, theta: t, phi: p });
       cartesian.z -= 3;
 
@@ -37,6 +67,8 @@ const toCartesian = (spheric: SphericCoords): Vec3 => {
 const render = (document: Document): void => {
   const ctx = getContext(document);
   renderSphere(ctx);
+  //   renderThetaSlice(ctx);
+  //   renderPhiSlice(ctx);
 };
 
 render(document);
